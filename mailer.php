9ec0f2d213303d6e7536a80c26ccab741d3894d1
Alex Nithitorn <alex.nithitorn@oppilab.net>
2026-08-28 08:15:49 +0300

    Lisätty salasana resetointi toiminto

--- mailer.php
+++ mailer.php
@@ -1,25 +1,25 @@
 <?php
 
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\SMTP;
-// use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-// require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/vendor/autoload.php";
 
-// $mail = new PHPMailer(true);
+$mail = new PHPMailer(true);
 
-// // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
-// $mail->isSMTP();
-// $mail->SMTPAuth = true;
+$mail->isSMTP();
+$mail->SMTPAuth = true;
 
-// $mail->Host = "smtp.example.com";
-// $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-// $mail->Port = 587;
-// $mail->Username = "your-user@example.com";
-// $mail->Password = "your-password";
+$mail->Host = "smtp.example.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+$mail->Username = "your-user@example.com";
+$mail->Password = "your-password";
 
-// $mail->isHtml(true);
+$mail->isHtml(true);
 
-// return $mail;
+return $mail;
 ?>
