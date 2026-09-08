5b87a6a8a1612e3369233f9a5aaa6f0e40fea7d2
Alex Nithitorn <alex.nithitorn@oppilab.net>
2026-09-08 13:11:14 +0300

    lisätyy excelin

--- config/mail.php
+++ config/mail.php
@@ -1,9 +1,9 @@
 <?php
 // Sähköpostipalvelimen konfiguraatio (Mail / SMTP Configuration)
-define('SMTP_HOST', 'smtp.resend.com');
+define('SMTP_HOST', 'smtp.example.com');
 define('SMTP_PORT', 587);
-define('SMTP_USER', 'resend');
-define('SMTP_PASS', 're_2MujKY9a_Ec2S6HbCycgMhcYT4odUw1mQ');
+define('SMTP_USER', 'exampleuser');
+define('SMTP_PASS', 'examplepassword');
 define('SMTP_SECURE', 'tls'); // 'tls' tai 'ssl'
 
 define('MAIL_FROM_ADDRESS', 'onboarding@resend.dev');
